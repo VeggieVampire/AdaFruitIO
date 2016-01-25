f8cb293d7b0ffaca48739c15d028ffc8eab622f4
Nick Farrow <VeggieVampire@users.noreply.github.com>
2016-01-25 05:10:21 -0600

    Run to setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+echo "updating/installing setuptools"
+sudo pip install --upgrade setuptools
+echo "updating/installing Adafruit_IO"
+sudo pip install Adafruit_IO
+
+
+echo -n "Enter your AIO KEY [ENTER]: "
+read AIOkey
+echo $AIOkey
+echo $AIOkey>key.cfg
+
+#Work in Progress (NOT WORKING YET)
+# Send a string value 'OFF' to the feed 'Red', creating it if it doesn't 
+# exist already.
+#aio.send('Red', 'OFF')
